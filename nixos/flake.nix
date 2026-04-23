f478203fa141218235b18460c696c0ac1ec0ede5
Floofy <53851147+FloofyIV@users.noreply.github.com>
2026-04-23 22:38:22 +1200

    a

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -2,11 +2,11 @@
   description = "nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    stablepkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -17,15 +17,12 @@
   };
 
   outputs =
-    { self, nixpkgs, nixpkgs-unstable, home-manager, helium, ... }@inputs:
+    { self, nixpkgs, home-manager, helium, ... }@inputs:
     {
     nixosConfigurations.nix = nixpkgs.lib.nixosSystem {
   system = "x86_64-linux";
 
   specialArgs = {
-    unstable = import nixpkgs-unstable {
-      system = "x86_64-linux";
-    };
     inherit inputs;
   };
 
